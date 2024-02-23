ed5b494efc2bf58599a65426d01c33fb1eb8b224
haileamlak <amlakhaile@gmail.com>
2024-02-24 00:36:04 +0300

    few edits

--- src/HeaderComponent.jsx
+++ src/HeaderComponent.jsx
@@ -10,7 +10,7 @@ export default function HeaderComponent() {
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" class="flex items-center">
                 <img src={logo} class="mr-3 h-9 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">Senay Tech</span>
+                <span class="self-center text-white text-xl font-semibold whitespace-nowrap dark:text-white">SenayCreatives</span>
             </a>
             <div class="flex items-center lg:order-2">
               <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600  bg-blue-400 focus:outline-none focus:ring-primary-800">Hire Us</a>
