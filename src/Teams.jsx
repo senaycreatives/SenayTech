b81f59c68fd3fa6450ae6d04d832083549fbb9bd
haileamlak <amlakhaile@gmail.com>
2024-02-27 11:54:26 +0300

    few edit

--- src/Teams.jsx
+++ src/Teams.jsx
@@ -7,9 +7,9 @@ export default function Teams() {
   const { data,error,isLoading}=UseFetchTeams()
   return (
     <div className='min-h-screen w-full'>
-        <div className=' w-full h-[50px] border-b-2 mt-[50px] mb-[50px]  border-white border-opacity-15'>
+         <div className=' w-full h-[50px] border-b-2 mt-[50px] mb-[50px]  border-white border-opacity-15'>
             <p className='text-white font-bold text-3xl'>OUR TEAMS</p>
-        </div>
+         </div>
     <div  class="  flex font-medium items-center flex-wrap  ">
       {isLoading&&
       <div className='w-full flex justify-center'>
