ec03eee2f630336efa777ee5e294d9b7be4a4165
andi-gech <andi.fab23@gmail.com>
2024-02-29 11:28:27 -0800

    bolgdate

--- src/BlogCard.jsx
+++ src/BlogCard.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import UsefetchUserDetails from './Hooks/UseFetchUser'
-
+import moment from 'moment';
 export default function BlogCard({blog}) {
   const {data,isLoading,error} = UsefetchUserDetails(blog?.Author)
+  const formattedDate = moment(blog?.Date).format('MMMM Do YYYY, h:mm:ss a');
   return (
     <Link  to={`/blogdetails/${blog?._id}`} className='  group sm:w-[80%] cursor-pointer hover:bg-black hover:bg-opacity-5   flex-row flex justify-between    mt-[50px] mb-[50px]  sm:pl-[100px]  border-opacity-15'>
     <div className='text-white max-w-[80%] flex flex-col'>
     <p className=' mb-2 font-bold text-[14px] italic  text-gray-300'>
         By {data?.data?.username}
+        
     </p>
-    
+    <p className=' mb-2 font-normal text-[14px] italic  text-gray-400'>
+     {formattedDate}
+        
+    </p>
     <p className='text-white font-bold text-xl mb-2 group-hover:text-orange-300 ' >{blog?.Title}</p>
     
     <p className='text-gray-300 line-clamp-3 '>{blog?.Description} </p>
